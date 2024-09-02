e392c5ca2804d6beab3ad09119fcc8565a87bfa5
adimiuprix <miuprix@gmail.com>
2024-09-02 13:06:52 +0700

    Create

--- src/app/prisma/buat/page.js
+++ src/app/prisma/buat/page.js
@@ -1,11 +1,50 @@
-'use client';
-import handleClick from "./action";
-function CreateUser() {
-    return (
-        <>
-            <button onClick={handleClick}>Click</button>
-        </>
-    );
-}
+'use client'
 
-export default CreateUser;
+import { useState } from 'react';
+
+export default function Home() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('/api/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email }),
+    });
+
+    const data = await res.json();
+    console.log('New user created:', data);
+  };
+
+  return (
+    <div>
+      <h1>Create User</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Create User</button>
+      </form>
+    </div>
+  );
+}
--- src/app/prisma/tampil/page.js
+++ src/app/prisma/tampil/page.js
@@ -1,20 +1,35 @@
 import prisma from "@/lib/prisma"
 
 async function loadData(){
+    // Menemukan semua data
     const dataUser = await prisma.user.findMany();
+
+    // Menemukan data dengan where
+    // const dataUser = await prisma.user.findMany({
+    //     where: {
+    //         email: 'garol@gmail.com'
+    //     }
+    // });
 
     return dataUser;
 }
 
 async function TampilData(){
     const user = await loadData()
+
     return(
-        <div className="card bg-base-100 w-96 shadow-xl">
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+        <>
+            <div className="flex">
+                {user.map((item, index) => (
+                <div key={index} className="card bg-base-100 w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">{item.email}</h2>
+                        <p>{item.name}</p>
+                    </div>
+                </div>
+                ))}
             </div>
-        </div>
+        </>
     )
 }
 
