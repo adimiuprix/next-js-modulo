0a3644242861fb4a7006db8ed6f5efe0adb8f9ac
adimiuprix <miuprix@gmail.com>
2024-09-30 21:55:29 +0700

    pake welcome

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <>
     <main className="container p-4 mx-auto mt-10">
         <div className="p-6 bg-white rounded-lg shadow-md">
-            <h1 className="mb-4 text-2xl font-bold text-red-800">Welcome to MyAppppppppppppp</h1>
+            <h1 className="mb-4 text-2xl font-bold text-red-800">Selamat datang di aplikasi Next js saya</h1>
             <p className="text-gray-700">This is a starter page created with Tailwind CSS. You can start building your application here.</p>
         </div>
     </main>
